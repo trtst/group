e5be6918805ca58b33b78e367a721aba9ab101fc
unknown <unknown@example.com>
2019-05-21 23:55:57 +0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,44 @@
+App({
+    onShow(options) {
+        const self = this;
+        const userInfo = wx.getStorageSync('userInfo') || {};
+        const isLogin = wx.getStorageSync('isLogin') || false;
+        const calcData = wx.getStorageSync('calcData') || {};
+
+        self.globalData.userInfo = userInfo;
+        self.globalData.isLogin = isLogin;
+        self.globalData.calcData = calcData;
+
+        if (JSON.stringify(self.globalData.calcData) == '{}') {
+            self.calc();
+        }
+    },
+    calc() {
+        const self = this,
+            systemInfo = wx.getSystemInfoSync(),
+            reg = /ios/i;
+
+        let pt = 20,         //导航状态栏上内边距
+            h = 44;          //导航状态栏高度
+
+        if (reg.test(systemInfo.system)) {
+            pt = systemInfo.statusBarHeight;
+            h = 44;
+        } else {
+            pt = systemInfo.statusBarHeight;
+            h = 48;
+        }
+
+        self.globalData.calcData = {
+            mainH: systemInfo.screenHeight - h - pt,
+            h: h,
+            pt: pt
+        }
+        wx.setStorageSync('calcData', self.globalData.calcData);
+    },
+    globalData: {
+        userInfo: {},
+        isLogin: false,
+        calcData: {}
+    }
+})
