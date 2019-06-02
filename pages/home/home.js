33e25a476530ef462a9d13887d13a6d31733f925
yangpenghui <569615439@qq.com>
2019-06-02 21:16:13 +0800

    添加首页

--- pages/home/home.js
+++ pages/home/home.js
@@ -4,25 +4,6 @@ const app = getApp();
 Page({
     data: {
         calcData: app.globalData.calcData,
-        maxH: app.globalData.calcData.mainH - 60 / 2 - 90 / 2,
-        x: 0,
-        currentX: 0
+        maxH: app.globalData.calcData.mainH - 60 / 2 - 90 / 2
     },
-    handleMovableChange(e) {
-        this.currentX = e.detail.x;
-    },
-    handleTouchend(e) {
-        if (this.currentX < -46) {
-            this.x = -92;
-            this.setData({
-                x: this.x
-            });
-        } else {
-            this.x = 0;
-            this.setData({
-                x: this.x
-            });
-        }
-    },
-    handleTouchestart(e) { }
 })
