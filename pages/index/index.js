7368d0213498d6a805acc54d7a8ee364dc4a2320
yangpenghui <yangpenghui@moer.cn>
2019-06-03 15:33:10 +0800

    首页布局提交

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,6 +3,6 @@ const app = getApp();
 Page({
     data: {
         calcData: app.globalData.calcData,
-        maxH: app.globalData.calcData.mainH - 60 / 2 - 90 / 2
+        maxH: app.globalData.calcData.mainH - 80 / 2 - 90 / 2
     },
 })
